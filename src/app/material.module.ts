ac3de95b591987dc9479c8e2d96907a69ad3487d
sahil n <sahil@mutterfly.in>
2018-08-09 17:56:18 +0530

    adds card layout

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,16 +3,16 @@ import {
   MatButtonModule, MatIconModule, MatFormFieldModule,
   MatInputModule, MatDatepickerModule, MatNativeDateModule,
   MatCheckboxModule, MatSidenavModule, MatToolbarModule,
-  MatListModule, MatTabsModule
+  MatListModule, MatTabsModule, MatCardModule
 
 } from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule,
             MatNativeDateModule, MatCheckboxModule, MatSidenavModule,
-            MatToolbarModule, MatListModule, MatTabsModule],
+            MatToolbarModule, MatListModule, MatTabsModule, MatCardModule],
   exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule,
             MatNativeDateModule, MatCheckboxModule, MatSidenavModule,
-          MatToolbarModule, MatListModule, MatTabsModule]
+          MatToolbarModule, MatListModule, MatTabsModule, MatCardModule]
 })
 export class MaterialModule {}
