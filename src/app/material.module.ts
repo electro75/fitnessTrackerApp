241073e6a85e3bd3447c939d3ba3faca25722488
sahil n <sahil@mutterfly.in>
2018-08-09 18:15:09 +0530

    populates new training

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,16 +3,19 @@ import {
   MatButtonModule, MatIconModule, MatFormFieldModule,
   MatInputModule, MatDatepickerModule, MatNativeDateModule,
   MatCheckboxModule, MatSidenavModule, MatToolbarModule,
-  MatListModule, MatTabsModule, MatCardModule
+  MatListModule, MatTabsModule, MatCardModule,
+  MatSelectModule
 
 } from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule,
             MatNativeDateModule, MatCheckboxModule, MatSidenavModule,
-            MatToolbarModule, MatListModule, MatTabsModule, MatCardModule],
+            MatToolbarModule, MatListModule, MatTabsModule, MatCardModule,
+            MatSelectModule],
   exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule,
             MatNativeDateModule, MatCheckboxModule, MatSidenavModule,
-          MatToolbarModule, MatListModule, MatTabsModule, MatCardModule]
+            MatToolbarModule, MatListModule, MatTabsModule, MatCardModule,
+            MatSelectModule]
 })
 export class MaterialModule {}
