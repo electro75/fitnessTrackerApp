6f126e73030ebfca4f8513ca4acda1cdb37bf6eb
sahil n <sahil@mutterfly.in>
2018-09-24 18:14:48 +0530

    adds universal snackbar method

--- src/app/shared/ui.service.ts
+++ src/app/shared/ui.service.ts
@@ -1,5 +1,15 @@
 import { Subject } from 'rxjs'
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 
+@Injectable()
 export class UIService { 
     loadingStateChanged = new Subject<boolean>();
+
+    constructor(private snackBar: MatSnackBar) {  }
+
+    showSnackbar(msg, action, duration) {
+        this.snackBar.open(msg, action, { duration })
+    }
+
  }
