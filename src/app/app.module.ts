faf561b3aa014bd628ea085bcc9116d917328c7d
sahil n <sahil@mutterfly.in>
2018-09-26 17:20:01 +0530

    adds different routing module for auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material.module';
+import { AngularFireModule } from '@angular/fire';
+import { AuthModule } from './auth/auth.module';
+import { TrainingModule } from './training/training.module';
 
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
-import { AngularFireModule } from '@angular/fire';
-
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './navigation/header/header.component';
@@ -16,8 +17,7 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
 import { AuthService } from './auth/auth.service';
 import { TrainingService } from './training/training.service';
 import { UIService } from './shared/ui.service';
-import { AuthModule } from './auth/auth.module';
-import { TrainingModule } from './training/training.module';
+
 
 
 @NgModule({
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from "@angular/forms";
 import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AuthRoutingModule } from '../auth/auth.routing.module';
 
 import { SignupComponent } from "./signup/signup.component";
 import { LoginComponent } from "./login/login.component";
@@ -14,9 +15,9 @@ import { LoginComponent } from "./login/login.component";
     ],
     imports: [
         SharedModule,
+        AuthRoutingModule,
         ReactiveFormsModule,
         AngularFireAuthModule,
-    ],
-    exports         : []
+    ]
 })
 export class AuthModule {  }
